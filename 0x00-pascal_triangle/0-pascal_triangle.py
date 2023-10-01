7134cb34ed4c89bf5a47bfb673f2e930cb2ae103
milemotigo <mtigo@gmail.com>
2023-10-01 11:45:19 -0700

    new commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,11 +6,14 @@ pascal triangle
 def pascal_triangle(n):
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
+
+    triangle = []
+    for i in range(n):
         row = [1]
-        last_row = triangle[-1]
-        row += [sum(pair) for pair in zip(last_row, last_row[1:])]
-        row.append(1)
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
         triangle.append(row)
     return triangle
