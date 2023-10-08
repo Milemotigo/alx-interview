9d2d7d8b9a14eb05fc4c3f2e5505e5062c19828b
milemotigo <mtigo@gmail.com>
2023-10-08 22:18:43 +0100

    new commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,36 @@ def canUnlockAll(boxes):
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
-    is_visited = [False] * len(boxes)
-    is_visited[0] = True
-    stack = [0]
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            # If the key can open an unvisited box
-            if not is_visited[key]:
-                is_visited[key] = True
-                stack.append(key)
-    return all(is_visited)
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
+
+# #!/usr/bin/python3
+# '''A module for working with lockboxes.
+# '''
+
+
+# def canUnlockAll(boxes):
+#     '''Checks if all the boxes in a list of boxes containing the keys
+#     (indices) to other boxes can be unlocked given that the first
+#     box is unlocked.
+#     '''
+#     is_visited = [False] * len(boxes)
+#     is_visited[0] = True
+#     stack = [0]
+#     while stack:
+#         current_box = stack.pop()
+#         for key in boxes[current_box]:
+#             # If the key can open an unvisited box
+#             if not is_visited[key]:
+#                 is_visited[key] = True
+#                 stack.append(key)
+#     return all(is_visited)
