7dafc096ea47224029a8dda671ac8531ef9eb5ca
milemotigo <mtigo@gmail.com>
2023-10-08 21:31:03 +0100

    new commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -20,11 +20,20 @@ def canUnlockAll(boxes):
     while stack:
         current_box = stack.pop()
 
-        if key in boxes[current_box]:
+        for key in boxes[current_box]:
             # If the key can open an unvisited box
             if not is_visited[key]:
                 is_visited[key] = True
 
                 stack.append(key)
         
-        return all(is_visited)
+    return all(is_visited)
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
