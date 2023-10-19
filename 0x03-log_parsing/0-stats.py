a9a9524d27bc91e7078c08131dba1ed040442ed8
milemotigo <mtigo@gmail.com>
2023-10-19 13:57:24 +0100

    task1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,37 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs.
 '''
+    script that reads stdin line by line and computes metrics
+'''
+import sys
 import re
+from collections import defaultdict
 
+total_size = 0
+status_counts = defaultdict(int)
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+line_count = 0
 
+try:
+    for line in sys.stdin:
+        line_count += 1
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+        match = re.match(r'(\S+) - \[([^\]]+)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)', line)
+        if match:
+            ip, date, status_code, file_size = match.groups()
+            status_code = int(status_code)
+            file_size = int(file_size)
 
+            total_size += file_size
+            status_counts[status_code] += 1
 
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
+        if line_count % 10 == 0:
+            print(f"Total file size: {total_size}")
+            for code in sorted(status_counts.keys()):
+                print(f"{code}: {status_counts[code]}")
 
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
+except KeyboardInterrupt:
+    pass
 
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
+print(f"Total file size: {total_size}")
+for code in sorted(status_counts.keys()):
+    print(f"{code}: {status_counts[code]}")
