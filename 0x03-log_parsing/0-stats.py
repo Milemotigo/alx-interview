b1a2207df76b2dc10d7e892a95c12b3714b263a2
milemotigo <mtigo@gmail.com>
2023-10-20 10:38:28 +0100

    status

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python3
-"""
-This module contains a method that reads stdin line by line and
-computes metrics
-"""
-
+"""log parsing"""
 import sys
+import re
 
+def match(x: str) -> bool:
+    """Checks for input match"""
+    ip_match = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
+    date_match = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] '
+    stats_match = r'"GET /projects/260 HTTP/1\.1" \d{3} \d+'
+    pattern = ip_match + date_match + stats_match
+    return True if re.match(pattern, x) else False
 
-def display_metrics(total_size, status_code):
-    """
-    Function that print the metrics
-    """
-
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_code.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
-
-
-if __name__ == '__main__':
+def log_parse() -> None:
+    """ A function that reads stdin line by line and computes metrics """
     total_size = 0
-    status_code = {
-        '200': 0, '301': 0,
-        '400': 0, '401': 0,
-        '403': 0, '404': 0,
-        '405': 0, '500': 0
+    status_codes = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
     }
+    
+    line_count = 0
 
     try:
-        i = 0
         for line in sys.stdin:
-            args = line.split()
-            if len(args) > 6:
-                status = args[-2]
-                file_size = args[-1]
-                total_size += int(file_size)
-                if status in status_code:
-                    i += 1
-                    status_code[status] += 1
-                    if i % 10 == 0:
-                        display_metrics(total_size, status_code)
+            if match(line):
+                a, b, c, d, e, f, g, status_code, f_size = line.split()
+                total_size += int(f_size)
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                line_count += 1
+                if line_count % 10 == 0:
+                    print(f'File size: {total_size}')
+                    for key, val in status_codes.items():
+                        if val > 0:
+                            print(f"{key}: {val}")
 
     except KeyboardInterrupt:
-        display_metrics(total_size, status_code)
-        raise
-    else:
-        display_metrics(total_size, status_code)
+        print(f'File size: {total_size}')
+        for key, val in status_codes.items():
+            if val > 0:
+                print(f"{key}: {val}")
+        exit(1)
+
+if __name__ == '__main__':
+    log_parse()
