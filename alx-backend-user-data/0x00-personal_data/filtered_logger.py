cc2d12a9dcc620320a97d731d9903feb66c71ab5
milemotigo <mtigo@gmail.com>
2023-11-03 00:22:14 +0100

    new_commit

--- alx-backend-user-data/0x00-personal_data/filtered_logger.py
+++ alx-backend-user-data/0x00-personal_data/filtered_logger.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
-"""0. Regex-ing"""
+"""
+module: filtered_logger.py
+"""
+import re
 
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
     """
-    Filters a message by obfuscating certain fields.
-
-    Args:
-        fields: A list of strings representing all fields to obfuscate.
-        redaction: A string representing by what the field will be obfuscated.
-        message: A string representing the log line.
-        separator: A string representing by which character is separating
-        all fields in the log line (message).
-
-    Returns:
-        A string representing the obfuscated log line.
+    filter_datum that returns the log message obfuscated
     """
     regex = '|'.join([re.escape(field) for field in fields])
     return re.sub(f'({regex})=[^{separator}]*', f'\\1={redaction}', message)
