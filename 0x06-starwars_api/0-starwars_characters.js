80a3ae7310c9cfed85adb27582a5c20c4bb21062
milemotigo <mtigo@gmail.com>
2023-11-08 21:03:56 +0100

    api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const film_id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${film_id}/`;
 
@@ -11,18 +9,17 @@ function fetch_star_wars() {
             console.log('Error');
         }
         let data = JSON.parse(res_body);
-        let characters = data.characters
+        let characters = data.characters;
         
         for (const characterUrl of characters) {
             request(characterUrl, function(error, responseChar, responseBody) {
                 if (error) {
-                    console.log('Error fetching data')
+                    console.log('Error fetching data');
                 }
-                const JSONResponse = JSON.parse(responseBody)
-                console.log(JSONResponse.name)
+                const JSONResponse = JSON.parse(responseBody);
+                console.log(JSONResponse.name);
             });
         }
     });
 }
-
 fetch_star_wars()
