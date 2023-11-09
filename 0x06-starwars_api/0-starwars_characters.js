89cbb86f8481dc0df9698eba49c7ed5d97c7bf01
milemotigo <mtigo@gmail.com>
2023-11-09 15:16:00 +0100

    new com

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
-const film_id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${film_id}/`;
 
-function fetch_star_wars() {
-    request(url, (error, response, res_body) => {
-        if (error) {
-            console.log('Error');
+const request = require('request');
+
+const film_id = process.argv[2];
+url = `https://swapi-api.alx-tools.com/api/films/${film_id}/`;
+
+function fetch_api() {
+    request(url, (error, response, body) => {
+        if (error){
+            console.log('error');
         }
-        let data = JSON.parse(res_body);
-        let characters = data.characters;
-        
-        for (const characterUrl of characters) {
-            request(characterUrl, function(error, responseChar, responseBody) {
-                if (error) {
-                    console.log('Error fetching data');
+        const res_json = JSON.parse(body);
+        const characters = res_json.characters;
+
+        for (const charactersUrl of characters) {
+            request(charactersUrl, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const char_json = JSON.parse(body);
+                    console.log(char_json.name);
                 }
-                const JSONResponse = JSON.parse(responseBody);
-                console.log(JSONResponse.name);
             });
         }
     });
 }
-fetch_star_wars()
+
+fetch_api()
