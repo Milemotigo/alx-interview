3d072b2ddb7341a7025146c021f9d47618588a91
milemotigo <mtigo@gmail.com>
2023-11-15 18:34:23 +0100

    new commit

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-'''0. Rotate 2D Matrix'''
+"""2D matrix rotation module.
+"""
 
 
 def rotate_2d_matrix(matrix):
-    '''
-    Do not return anything. The matrix must be edite
-    in-place.
-    '''
-    n = len(matrix)
-
-    for i in range(n):
-        for j in range(i, n):
-            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-        for i in range(n):
-            matrix[i].reverse()
+    """Rotates an m by n 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
