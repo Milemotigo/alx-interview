396725db07f4cd5cb0579087f28153d6cdf98af7
milemotigo <mtigo@gmail.com>
2023-12-03 18:15:27 +0100

    new interview

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+def island_perimeter(grid):
+    perimeter = 0
+
+    if not grid or not grid[0]:
+        return perimeter
+
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
